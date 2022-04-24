ae2932ef8ff46b7f0d00e3ad4f9c4710a0a94c16
gerardo_nevarez <GerardoNevarez@users.noreply.github.com>
2022-04-24 18:46:18 -0400

    Re-enable QMK settings, change unlock combo, disable bootmagic lite & magic keycodes

--- keyboards/ferris/keymaps/vial/config.h
+++ keyboards/ferris/keymaps/vial/config.h
@@ -4,6 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0x47, 0x97, 0x7E, 0x32, 0xC9, 0xAC, 0x13, 0xF3}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 2, 3, 0, 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1, 0, 2, 4 }
-
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0, 4 }
